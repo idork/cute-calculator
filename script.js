6417141e19233778c3c40df5bb49164bc674df60
idork <idettemonterroza@gmail.com>
2021-01-09 01:11:56 -0500

    chore: made span blink

--- script.js
+++ script.js
@@ -5,5 +5,16 @@ window.addEventListener('keydown',(e) => {
     console.log(e);
 })
 
+var cursor = true;
+var speed= 250;
+
+setInterval(() => {
+  if (cursor) { document.getElementById('cursor').style.opacity =0;
+  cursor = false;
+}else {        document.getElementById('cursor').style.opacity = 1;
+  cursor = true;
+      }
+}, speed);
+
 
 
